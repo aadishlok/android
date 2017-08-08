0caa3bde286c06ff0a1b2f73b35314ef8540aa35
Aadi Shlok <shlok.a@husky.neu.edu>
2017-08-08 09:41:45 -0400

    Firebase Facebook Login update: Profile Pic Dimension Expansion

--- FirebaseFacebookLogin/app/src/main/java/com/sio/ffl/MainActivity.java
+++ FirebaseFacebookLogin/app/src/main/java/com/sio/ffl/MainActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.Profile;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity{
     private List<AuthUI.IdpConfig> providers;
     private FirebaseAuth auth;
     private Button logout;
+    private TextView name;
+    private ImageView displayPic;
 
     private static final int RC_SIGN_IN= 0;
 
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         logout= (Button) findViewById(R.id.logout);
+        name= (TextView) findViewById(R.id.name);
 
         providers= new ArrayList<>();
         providers.add(new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
@@ -69,7 +76,10 @@ public class MainActivity extends AppCompatActivity{
         if(requestCode == RC_SIGN_IN){
             if(resultCode == RESULT_OK){
                 // user logged in
-                Log.d("AUTH", auth.getCurrentUser().getEmail()+" logged in.");
+                name.setText(auth.getCurrentUser().getDisplayName().toString());
+                Profile profile= Profile.getCurrentProfile();
+                new DownloadImage((ImageView) findViewById(R.id.displayPic)).execute(profile.getProfilePictureUri(400,400).toString());
+                Log.d("AUTH", auth.getCurrentUser().getEmail()+"\n"+auth.getCurrentUser().getPhotoUrl().toString()+"\n"+profile.getName());
             }else{
                 Log.d("AUTH", "Login Failed");
                 finish();
