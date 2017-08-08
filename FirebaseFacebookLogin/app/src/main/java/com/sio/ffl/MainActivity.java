f03cb6a43191b37895d296116f89490e5bbdc542
Aadi Shlok <shlok.a@husky.neu.edu>
2017-08-08 08:45:58 -0400

    Firebase Facebook Login first commit

---
+++ FirebaseFacebookLogin/app/src/main/java/com/sio/ffl/MainActivity.java
@@ -0,0 +1,79 @@
+package com.sio.ffl;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth auth;
+    private Button logout;
+
+    private static final int RC_SIGN_IN= 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logout= (Button) findViewById(R.id.logout);
+
+        providers= new ArrayList<>();
+        providers.add(new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+        auth= FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null){
+            // already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail()+" logged in.");
+        }else{
+            // not signed in
+            startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build(), RC_SIGN_IN);
+        }
+
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(v.getId() == R.id.logout){
+                    AuthUI.getInstance()
+                            .signOut(MainActivity.this)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Log.d("AUTH", "User logged out.");
+                                    finish();
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                // user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail()+" logged in.");
+            }else{
+                Log.d("AUTH", "Login Failed");
+                finish();
+            }
+        }
+    }
+}
